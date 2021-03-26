0f8c9b6cb6714d7ce331df328175864eafe13b46
GanaxXD <pedro.mattis.df@gmail.com>
2021-03-26 18:08:28 -0300

    Finalização

---
+++ src/main/java/com/example/demo/representationmodel/OrdemServicoRepresentationModel.java
@@ -0,0 +1,15 @@
+package com.example.demo.representationmodel;
+/*
+ * A Representation model será o modelo de representação das
+ * models lançadas.
+ * O Objetivo é que durante a utilização da API, caso eu 
+ * precise mostrar apenas alguns dados da model nos 
+ * resultados dos endpoints, eu utilize as representationsmodels
+ * e não as models. Ao criá-las, o tipo de retorno dos métiodos
+ * no controller e nos services devem ser dessas models.
+ * Para mapeá-las na utilização, foi usada a lib modelmapper
+ * que facilita o processo.
+ */
+public class OrdemServicoRepresentationModel {
+
+}
