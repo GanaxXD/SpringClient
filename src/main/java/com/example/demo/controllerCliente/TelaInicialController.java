793e00a658f8c2c80ba254cc4f30be1d240740bd
GanaxXD <pedro.mattis.df@gmail.com>
2021-05-04 22:34:01 -0300

    Tentando reestabelecer a página index

--- src/main/java/com/example/demo/controllerCliente/TelaInicialController.java
+++ src/main/java/com/example/demo/controllerCliente/TelaInicialController.java
@@ -1,12 +1,14 @@
 package com.example.demo.controllerCliente;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping(value="/", method = RequestMethod.GET)
 public class TelaInicialController {
+	
 	public ModelAndView index() {
 		return new ModelAndView("index.html");
 	}
