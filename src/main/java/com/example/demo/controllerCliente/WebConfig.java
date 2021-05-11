8ffeea9db3cdcbde1a4dda536d4c7b4ff43fdba1
GanaxXD <pedro.mattis.df@gmail.com>
2021-05-11 20:58:56 -0300

    inserção do WebConfig class no pacote controllerClient

---
+++ src/main/java/com/example/demo/controllerCliente/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.controllerCliente;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//Por padrão, o Spring nega requisições do tipo Option, por isso, é preferível 
+		//adicionar logo todos os métodos, como no exemplo ativo:
+		//registry.addMapping("/**");
+		
+		registry.addMapping("/**")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", 
+							"TRACE", "CONNECT");
+	}
+}
