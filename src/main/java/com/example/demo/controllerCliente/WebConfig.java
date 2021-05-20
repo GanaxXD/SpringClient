7b8d8c922fd02c347f5761a9c314251ba5253f52
GanaxXD <pedro.mattis.df@gmail.com>
2021-05-19 21:58:50 -0300

    inserindo o allowedOrigins na WebConfig.java pra tentar corrigir o acesso ao método por qualquer origem

--- src/main/java/com/example/demo/controllerCliente/WebConfig.java
+++ src/main/java/com/example/demo/controllerCliente/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig implements WebMvcConfigurer{
 		
 		registry.addMapping("/**")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", 
-							"TRACE", "CONNECT");
+							"TRACE", "CONNECT")
+			.allowedOrigins("*");
 	}
 }
